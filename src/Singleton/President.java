61cb9fe9ea124603c1c46df52693865a6df03b3c
Amir Nasiri <amirnasiri@hotmail.co.uk>
2018-08-23 06:46:28 +0100

    singleton done.

---
+++ src/Singleton/President.java
@@ -0,0 +1,27 @@
+package Singleton;
+
+public class President {
+	private static President current;
+	private String name;
+
+	private President(String name) {
+		this.name = name;
+	}
+
+	public static President getPresident() {
+		if (current == null) {
+			current = new President("");
+		}
+		return current;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
